fc78182ddc58183a119cdecb5e4ccf7602a1478b
Peter Sbarski <peter.sbarski@gmail.com>
2013-10-13 14:28:26 +1100

    adding missing file

---
+++ src/SSOApp/Extensions/SSOSignOn.cs
@@ -0,0 +1,31 @@
+﻿using System.Web.Security;
+using SSOApp.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebMatrix.WebData;
+
+namespace SSOApp.Extensions
+{
+    public class SSOSignOn : AuthorizeAttribute
+    {
+        protected override bool AuthorizeCore(HttpContextBase httpContext)
+        {
+            var cookie = httpContext.Request.Cookies["sso"];
+
+            if (cookie != null)
+            {
+                var token = cookie.Value;
+                var username = SSOAuthenticationService.DecryptToken(token);
+
+                if (WebSecurity.UserExists(username))
+                {
+                    FormsAuthentication.SetAuthCookie(username, false);
+                }
+            }
+            return base.AuthorizeCore(httpContext);
+        }
+    }
+}
